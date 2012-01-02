28b168f28041f491b466e9bb94acdeacc3c1d673
Krishna Kumar <krishnau2@gmail.com>
2012-01-03 01:47:21 +0530

    Initial commit Created rails 3.1 project mysql2 database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ketogenic::Application.config.secret_token = '6767aecec8b9c075f719fddab151a3ea95d5e083033864264c3a79eb2f4958ac13eac9d070cf797df61622b51460da1feed31d830f9d6202576911bc2a4fc9b1'

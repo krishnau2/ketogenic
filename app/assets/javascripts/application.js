72516680004d5727630945c4734ccf39522ccf57
Krishna Kumar <krishnau2@gmail.com>
2012-01-04 02:10:37 +0530

    First calculation. Only for eggs calorie calculation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,17 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+$(document).ready(function(){
+	var egg_protein = "6.3"
+	var egg_fat = "5.3"
+	var egg_carbo = "0.56"
+	var calories_of_1_egg = ((egg_protein * 4)+(egg_fat * 9)+(egg_carbo * 4))
+
+	$("#find").click(function(){
+		var no_eggs = $("#eggs").val();
+		var total_calories = (no_eggs * calories_of_1_egg);
+		var msg = "Total calores for "+no_eggs+" eggs : ";
+		msg += total_calories.toFixed(2);
+		$("#result").html(msg)
+	});
+});
